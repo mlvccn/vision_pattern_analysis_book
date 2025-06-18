b2842e4b39bccdde70fbfed8b106c909b1b7a8e0
HeLingF <117101003+HFeliks@users.noreply.github.com>
2025-06-18 10:25:22 +0800

    Add files via upload

---
+++ CH5-时空动作检测/时空动作检测任务实例/pi-consistency-activity-detection-demo/datasets/path.py
@@ -0,0 +1,17 @@
+#============↓↓↓↓↓↓↓↓↓↓↓↓=============JHMDB数据集路径=========↓↓↓↓↓↓↓↓↓↓↓↓==============
+# Dataset directories
+jhmdb_dataset_dir = 'DATASETS/JHMDB/data/Videos/ReCompress_Videos/'
+jhmdb_mask_dir = 'DATASETS/JHMDB/data/Puppet mask/puppet_mask/'
+
+# Annotation files base path
+jhmdb_annot_base_path = 'DATASETS/JHMDB/jhmdb_txt_file/'
+
+#============↑↑↑↑↑↑↑↑↑↑↑↑=============JHMDB数据集路径=========↑↑↑↑↑↑↑↑↑↑↑↑==============
+
+#============↓↓↓↓↓↓↓↓↓↓↓↓=============UCF101-24数据集路径=========↓↓↓↓↓↓↓↓↓↓↓↓==============
+# UCF101 dataset directory
+ucf101_dataset_dir = 'DATASETS/UCF101-24/UCF-101'
+
+# UCF101-24 annotation files base path
+ucf101_annot_base_path = 'DATASETS/UCF101-24/ucf101-24_pkl_file/'
+#============↑↑↑↑↑↑↑↑↑↑↑↑=============UCF101-24数据集路径=========↑↑↑↑↑↑↑↑↑↑↑↑==============
